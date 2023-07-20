Fixed p_host normalization

DMDLinference/corner_plots.py
@@ -14,13 +14,13 @@ import matplotlib.lines as mlines
 import seaborn as sns
 
 # Load results from inference on simulated FRB-GW events.
-filename = "../Data/simulated_10FRBs_100x50000steps.h5"
+filename = "../Data/simulated_10FRBs_z1_24x500steps.h5"
 sampler = emcee.backends.HDFBackend(filename)
 
-# tau = sampler.get_autocorr_time()
-# burnin = int(2 * np.max(tau))
+tau = sampler.get_autocorr_time()
+burnin = int(2 * np.max(tau))
 
-samples = sampler.get_chain(discard=10000)
+samples = sampler.get_chain(discard=burnin)
 
 # Results from only James
 filename1 = "../Data/James_prior_100x50000steps.h5"
@@ -32,13 +32,13 @@ burnin = int(2 * np.max(tau))
 samples_J = sampler_J.get_chain(discard=burnin)
 
 # Results without taking James et al. into account.
-filename2 = "../Data/simulated_noz_10FRBs_100x50000steps.h5"
+filename2 = "../Data/simulated_noz_10FRBs_z1_24x500steps.h5"
 sampler_noz = emcee.backends.HDFBackend(filename2)
 
-# tau = sampler_noz.get_autocorr_time()
-# burnin = int(2 * np.max(tau))
+tau = sampler_noz.get_autocorr_time()
+burnin = int(2 * np.max(tau))
 
-samples_noz = sampler_noz.get_chain(discard=10000)
+samples_noz = sampler_noz.get_chain(discard=burnin)
 
 # Plot corner plot with all DMs and D_Ls
 # labels=(['$H_0$', r'$\Omega_b f_d$']
@@ -46,6 +46,7 @@ samples_noz = sampler_noz.get_chain(discard=10000)
 #         + [r"DM$_\mathrm{host}$"+f"{str(frb)}" for frb in range(n_FRBs)]
 #         )
 # fig = corner.corner(sampler, labels=labels, truths=[H0, Obf, *DL_meas, *DM_host])
+
 labels=(['$H_0$', r'$\Omega_b f_d$'])
 
 cm2 = sns.color_palette('deep') #plt.get_cmap('tab10')
@@ -70,7 +71,7 @@ plot_kwargs = dict(labels=labels,
                    )
 fig = corner.corner(samples_J.swapaxes(0,1),
                     color=cm2[2],
-                    hist_kwargs={'density' : True, 'lw' : 2.},
+                    hist_kwargs={'density' : True, 'lw' : 2., 'label' : "FRB-z priors"},
                     contour_kwargs={'linewidths' : .5, 'colors' : [cm2[2]]},
                     contourf_kwargs={'colors' : color_set[0],},
                     **plot_kwargs,
@@ -78,7 +79,7 @@ fig = corner.corner(samples_J.swapaxes(0,1),
 fig = corner.corner(samples_noz[:,:,:2].swapaxes(0,1),
                     color=cm2[1],
                     fig=fig,
-                    hist_kwargs={'density' : True, 'lw' : 2.},
+                    hist_kwargs={'density' : True, 'lw' : 2., 'label' : "FRB-GW constraints only"},
                     contour_kwargs={'linewidths' : .5, 'colors' : [cm2[1]]},
                     contourf_kwargs={'colors' : color_set[1],},
                     **plot_kwargs,
@@ -86,24 +87,25 @@ fig = corner.corner(samples_noz[:,:,:2].swapaxes(0,1),
 fig = corner.corner(samples[:,:,:2].swapaxes(0,1),
                     color=cm2[0],
                     fig=fig,
-                    hist_kwargs={'density' : True, 'lw' : 2.},
+                    hist_kwargs={'density' : True, 'lw' : 2., 'label' : "Combined constraints"},
                     contour_kwargs={'linewidths' : .5, 'colors' : [cm2[0]]},
                     contourf_kwargs={'colors' : color_set[2],},
                     **plot_kwargs
                     )
-sample_labels = ["Combined constraints",
-                 "FRB-GW constraints only",
-                 "FRB-z priors"
-                 ]
-plt.legend(
-        handles=[
-            mlines.Line2D([], [], color=cm2[i], label=sample_labels[i])
-            for i in range(len(sample_labels))
-        ],
-        # fontsize=20, frameon=False,
-        bbox_to_anchor=(1, 2),
-        loc="upper right"
-    )
+# sample_labels = ["Combined constraints",
+#                  "FRB-GW constraints only",
+#                  "FRB-z priors"
+#                  ]
+plt.legend(bbox_to_anchor=(1.05, 2), loc="upper right")
+# plt.legend(
+#         handles=[
+#             mlines.Line2D([], [], color=cm2[i], label=sample_labels[i])
+#             for i in range(len(sample_labels))
+#         ],
+#         # fontsize=20, frameon=False,
+#         bbox_to_anchor=(1.15, 2),
+#         loc="upper right"
+#     )
 
 fig_path = os.path.splitext(filename)[0] + ".png"
 fig.savefig(fig_path, bbox_inches='tight', pad_inches=0.01, dpi=300)

DMDLinference/mcmc.py
@@ -84,12 +84,6 @@ DLum = posterior_samples['luminosity_distance']
 p_DL_tmp = gaussian_kde(DLum)
 p_DL = lambda DL, *args: p_DL_tmp(DL)
 
-# Fixing values to the ones reported by James et al. 2022
-# (i.e. using delta function priors instead of marginalizing)
-mu_host = 2.23
-sigma_host = 0.57
-F = 0.32
-#f_igm = 0.83
 
 def lum_dist(z, Om=0.3):
     """The luminosity distance for a flat LCDM universe in units of the hubble distance"""
@@ -258,13 +252,14 @@ def log_p_DM(DMexc, DMhost, DL, H0, Obf):
     # integ = quad(p_product, 0.001, DMexc-0.001)
     return np.sum(log_p)
 
+
 def p_DM(DL, H0, Obf):
     """Probability measureing a DM given the distance and parameters."""
     z = z_of_DL(DL, H0)
     if z.shape:
         z = z[:, np.newaxis]
 
-    p_DMhost = log10_normal(DMcosmic[..., ::-1]*(1+z), mu=mu_host, sigma=sigma_host)
+    p_DMhost = (1+z)*log10_normal(DMcosmic[..., ::-1]*(1+z), mu=mu_host, sigma=sigma_host)
     integral = np.sum(p_DMhost*p_DMcosmic(DMcosmic, z, F, H0, Obf, Om=0.3), axis=-1)*dDM
 
     return integral
@@ -357,6 +352,14 @@ def log_p_H0_with_prior(theta):
 
 if __name__ == '__main__':
     DMexc_ne2001 = 79.4
+
+    # Fixing values to the ones reported by James et al. 2022
+    # (i.e. using delta function priors instead of marginalizing)
+    mu_host = 2.23
+    sigma_host = 0.57
+    F = 0.32
+    #f_igm = 0.83
+
     nwalkers = 24
     rng = np.random.default_rng()
     # DL_init = rng.normal(DLum.mean(), 10, size=nwalkers)

DMDLinference/simulate_data.py
@@ -135,7 +135,7 @@ def p_DL(DL_samps, **kwargs):
 
 if __name__ == '__main__':
     # Simulate FRBs.
-    n_FRBs = 100
+    n_FRBs = 10
 
     c = 299792.458
     Obf = .049*0.844
@@ -146,6 +146,10 @@ if __name__ == '__main__':
     DM0 = 10**mu_host
     sigma_host = 0.57
     F = 0.32
+    mcmc.mu_host = mu_host
+    mcmc.sigma_host = sigma_host
+    mcmc.F = F
+
     DL_mean = c/H0*lum_dist(z_mean, Om=0.3)
     sigma_DL = 0.1*DL_mean
     DL_meas, DMexc, DM_host = simulate_FRBs(n_FRBs, z_mean=z_mean, z_sigma=0, sigma_DL=sigma_DL, Obf=Obf,
@@ -169,7 +173,7 @@ if __name__ == '__main__':
     initial = np.concatenate((H0_init, Obf_init), axis=1)
 
     ndim = 2
-    nsteps = 50
+    nsteps = 500
 
     # Set up a backend to save the chains to.
     filename = f"../Data/simulated_{n_FRBs}FRBs_z{z_mean}_{nwalkers}x{nsteps}steps.h5"
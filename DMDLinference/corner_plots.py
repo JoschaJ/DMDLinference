3e19da2eadf3305733d0cfced9e0af019f4def87
JoschaJ <jjahns@mpifr-bonn.mpg.de>
2024-01-05 06:41:32 +0100

    Just small renaming and an addition dir to ignore.

--- DMDLinference/corner_plots.py
+++ DMDLinference/corner_plots.py
@@ -16,31 +16,31 @@ import seaborn as sns
 import config
 
 # Load results from inference on simulated FRB-GW events.
-filename = os.path.join(config.DATA_DIR, "simulated_10FRBs_z0.1_24x10000steps.h5")
+filename = os.path.join(config.DATA_DIR, "simulated_10FRBs_z0.1_eDL0.4_24x1000steps.h5")
 sampler = emcee.backends.HDFBackend(filename)
 
 #tau = sampler.get_autocorr_time()
 #burnin = int(2 * np.max(tau))
 
-samples = sampler.get_chain(discard=1000)
+samples = sampler.get_chain(discard=100)
 
 # Results from only James
-filename1 = os.path.join(config.DATA_DIR, "James_prior_100x50000steps.h5")
+filename1 = os.path.join(config.DATA_DIR, "James_prior_24x50000steps.h5")
 sampler_J = emcee.backends.HDFBackend(filename1)
 
-tau = sampler_J.get_autocorr_time()
-burnin = int(2 * np.max(tau))
+# tau = sampler_J.get_autocorr_time()
+# burnin = int(2 * np.max(tau))
 
-samples_J = sampler_J.get_chain(discard=burnin)
+samples_J = sampler_J.get_chain(discard=1000)
 
 # Results without taking James et al. into account.
-filename2 = os.path.join(config.DATA_DIR, "simulated_noz_10FRBs_z0.1_24x10000steps.h5")
+filename2 = os.path.join(config.DATA_DIR, "simulated_noz_10FRBs_z0.1_eDL0.4_24x1000steps.h5")
 sampler_noz = emcee.backends.HDFBackend(filename2)
 
 #tau = sampler_noz.get_autocorr_time()
 #burnin = int(2 * np.max(tau))
 
-samples_noz = sampler_noz.get_chain(discard=1000)
+samples_noz = sampler_noz.get_chain(discard=100)
 
 # Plot corner plot with all DMs and D_Ls
 # labels=(['$H_0$', r'$\Omega_b f_d$']
@@ -69,7 +69,7 @@ plot_kwargs = dict(labels=labels,
                    plot_density=False,
                    plot_datapoints=False,
                    fill_contours=True,
-                   range=[(10, 150), (0, 0.2)],
+                   range=[(10, 150), (0, 0.06)],
                    )
 fig = corner.corner(samples_J.swapaxes(0,1),
                     color=cm2[2],
@@ -78,7 +78,7 @@ fig = corner.corner(samples_J.swapaxes(0,1),
                     contourf_kwargs={'colors' : color_set[0],},
                     **plot_kwargs,
                     )
-fig = corner.corner(samples_noz[:,:,:2].swapaxes(0,1),
+fig = corner.corner(samples_noz.swapaxes(0,1),
                     color=cm2[1],
                     fig=fig,
                     hist_kwargs={'density' : True, 'lw' : 2., 'label' : "FRB-GW constraints only"},
@@ -86,7 +86,7 @@ fig = corner.corner(samples_noz[:,:,:2].swapaxes(0,1),
                     contourf_kwargs={'colors' : color_set[1],},
                     **plot_kwargs,
                     )
-fig = corner.corner(samples[:,:,:2].swapaxes(0,1),
+fig = corner.corner(samples.swapaxes(0,1),
                     color=cm2[0],
                     fig=fig,
                     hist_kwargs={'density' : True, 'lw' : 2., 'label' : "Combined constraints"},
